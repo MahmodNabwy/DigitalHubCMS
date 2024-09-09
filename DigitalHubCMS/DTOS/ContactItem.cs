b5366959e489d1d6323b2f72446fa78715fe77f8
Mahmoud Elnabwy <mahmoud.ashraf@digitalhub.com.eg>
2024-09-09 17:19:22 +0300

    working on footer

---
+++ DigitalHubCMS/DTOS/ContactItem.cs
@@ -0,0 +1,10 @@
+﻿namespace DigitalHubCMS.DTOS
+{
+    public class ContactItem
+    {
+        public int Id { get; set; }
+        public string Icon { get; set; }  
+        public string Label { get; set; }
+        public string Link { get; set; }
+    }
+}
